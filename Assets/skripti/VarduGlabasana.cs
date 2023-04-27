5b37481479fee7abf1d08cc5ae3632d9c1d25e9c
Pavlovskis <kristianspavlovskis@gmail.com>
2023-04-27 12:14:45 +0300

    vardu uzglabasana ar pogas palidzibu

---
+++ Assets/skripti/VarduGlabasana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VarduGlabasana : MonoBehaviour {
+
+	private string teksts;
+	public GameObject ievadesLauks;
+	public GameObject tekstaAttelosana;
+
+
+	public void UzglabatTekstu(){
+		
+		teksts = ievadesLauks.GetComponent<InputField>().text;
+		tekstaAttelosana.GetComponent<Text>().text = teksts;
+	}
+}
