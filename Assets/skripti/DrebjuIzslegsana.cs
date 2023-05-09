6d7afc4d81721eac22e58a5fc78482e7bb5ab899
Kristians <kristianspavlovskis@gmail.com>
2023-05-09 18:14:12 +0300

    izņemti dažas nevajadzīgas lietas un pareizi nobuildoju

---
+++ Assets/skripti/DrebjuIzslegsana.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrebjuIzslegsana : MonoBehaviour {
+
+	public GameObject CimduBilde;
+    public GameObject BiksesBilde;
+    public GameObject KurpesBilde;
+    public GameObject KrekluBilde;
+    public GameObject MatuBilde;
+
+
+public void Cimdi()
+{
+    CimduBilde.SetActive(!CimduBilde.activeSelf);
+}
+public void Bikses()
+{
+    BiksesBilde.SetActive(!BiksesBilde.activeSelf);
+}
+public void Kurpes()
+{
+    KurpesBilde.SetActive(!KurpesBilde.activeSelf);
+}
+public void Krekli()
+{
+    KrekluBilde.SetActive(!KrekluBilde.activeSelf);
+}
+public void Mati()
+{
+    MatuBilde.SetActive(!MatuBilde.activeSelf);
+}
+
+}
